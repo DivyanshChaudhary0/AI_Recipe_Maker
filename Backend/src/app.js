2d87075c9eb924f57d0fee3fb9d313e3ccf970cc
Divyansh chaudhary <hypr123890@gmail.com>
2025-05-15 12:07:45 +0530

    Add user registration and login controller with error handling

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,12 +2,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors({
-    origin: "*",
-    credentials: true
-}))
+app.use(cors({origin: "*", credentials: true}))
+app.use(cookieParser())
 
 module.exports = app;
---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,70 @@
+
+const userModel = require("../models/user.model");
+
+const registerUser = async function(req,res){
+    try{
+        const {username,email,password} = req.body;
+
+        if(!username || !email || !password){
+            return res.status(400).json({
+                message: "Please fill all data"
+            })
+        }
+
+        const isAlredayExist = await userModel.findOne({
+            $or: [{username},{email}]
+        })
+
+        if(isAlredayExist){
+            return res.status(400).json({
+                message: "User already exist"
+            })
+        }
+
+        const hashedPassword = await userModel.hashPassword(password);
+
+        const user = await userModel.create({
+            username,
+            email,
+            password: hashedPassword
+        })
+
+        const token = user.generateToken();
+
+        res.cookie("token", token, {
+            httpOnly: true,       
+            secure: true,         
+            sameSite: "None", 
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+
+        res.status(201).json({
+            message: "User created successfully",
+            user
+        })
+
+    }
+    catch(err){
+        return res.status(500).json({
+            error: err.message,
+            message: "Internal server error"
+        })
+    }
+}
+
+const loginUser = async function(req,res){
+    try{
+        
+    }
+    catch(err){
+        res.status(500).json({
+            error: err.message,
+            message: "Internal server error"
+        })
+    }
+}
+
+module.exports = {
+    registerUser,
+    loginUser
+}
