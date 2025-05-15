0da4545313c0c86fdc1bf66deb5a66717916f4db
Divyansh chaudhary <hypr123890@gmail.com>
2025-05-15 15:22:15 +0530

    feat: implement login and registration forms with state management

--- Frontend/src/views/login/Login.jsx
+++ Frontend/src/views/login/Login.jsx
@@ -1,13 +1,33 @@
 
-import { LoginForm } from "@/components/login-form"
+import { useState } from 'react'
 
-export default function LoginPage() {
+const Login = () => {
+
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <LoginForm />
-      </div>
+    <div className='w-full h-screen flex flex-col gap-4 items-center justify-center'>
+        <h2>Login User</h2>
+        <form className='flex flex-col gap-3 w-[400px]'>
+            <input 
+                type="text"  
+                placeholder='Enter email' 
+                className='border border-gray-500 rounded-xl outline-blue-500 px-4 py-2' 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input 
+                type="text" 
+                placeholder='Enter password' 
+                className='border border-gray-500 rounded-xl outline-blue-500  px-4 py-2' 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+            />
+            <button className='px-2 py-2 bg-blue-500 hover:bg-blue-600 transition-all text-white rounded-xl cursor-pointer'>Login</button>
+        </form>
     </div>
   )
 }
 
+export default Login
