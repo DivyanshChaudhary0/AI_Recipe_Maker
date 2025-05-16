d90adc7d8962252a95b30e4aa51c99ac14d80cbb
Divyansh chaudhary <hypr123890@gmail.com>
2025-05-16 20:59:21 +0530

    fix: format code in Protected component for consistency and add missing newline at end of file

--- Frontend/src/components/Protected.jsx
+++ Frontend/src/components/Protected.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import { useEffect } from 'react'
 import { BASE_URL } from '../utils/constants'
-import {useDispatch} from "react-redux"
+import { useDispatch } from "react-redux"
 import { addUser } from '../store/userSlice'
 
 const Protected = ({children}) => {
@@ -10,6 +10,7 @@ const Protected = ({children}) => {
     const dispatch = useDispatch();
 
     useEffect(() => {
+
         axios.get(BASE_URL + "/api/v1/user/me", { withCredentials: true })
         .then((res) => {
             dispatch(addUser(res.data.user));
@@ -22,4 +23,4 @@ const Protected = ({children}) => {
   return children
 }
 
-export default Protected
+export default Protected;
