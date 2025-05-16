c8b2e43ab91fbb67010bd7da39024e2c08b32d0a
Divyansh chaudhary <hypr123890@gmail.com>
2025-05-16 20:50:59 +0530

    feat: add Redux Toolkit and React-Redux for state management in Protected component

--- Frontend/src/components/Protected.jsx
+++ Frontend/src/components/Protected.jsx
@@ -1,10 +1,22 @@
 
-import React, { useEffect } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { BASE_URL } from '../utils/constants'
+import {useDispatch} from "react-redux"
+import { addUser } from '../store/userSlice'
 
-const Protected = (children) => {
+const Protected = ({children}) => {
+
+    const dispatch = useDispatch();
 
     useEffect(() => {
-        
+        axios.get(BASE_URL + "/api/v1/user/me", { withCredentials: true })
+        .then((res) => {
+            dispatch(addUser(res.data.user));
+        })
+        .catch((err) => {
+            console.log(err);
+        })
     },[])
 
   return children
